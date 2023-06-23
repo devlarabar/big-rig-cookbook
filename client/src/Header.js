bddbdc21407fae80cd8f298c6745873e5d73462b
Lara Alexander <lara.alexander@hotmail.com>
2023-06-23 17:30:03 -0400

    removed unused import

--- client/src/Header.js
+++ client/src/Header.js
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom'
-import { useEffect, useState, useContext } from 'react'
+import { useEffect, useContext } from 'react'
 import { UserContext } from './UserContext'
 
 const Header = () => {
