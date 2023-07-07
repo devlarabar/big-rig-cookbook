b4f521f4dfcfc08ff99de3cef06ce83b3422aa8e
Lara Alexander <lara.alexander@hotmail.com>
2023-07-07 10:22:57 -0400

    added time to posts and styled it

--- client/src/Post.js
+++ client/src/Post.js
@@ -2,14 +2,22 @@ import { format } from 'date-fns'
 import { Link } from 'react-router-dom'
 import SaveButton from './SaveButton'
 
-const Post = ({ _id, title, summary, cover, content, author, createdAt, savedBy, userId, userDetails }) => {
+const Post = ({ _id, title, summary, cover, content, author, prepTime, cookTime, createdAt, savedBy, userId, userDetails }) => {
+	let clockStyle
+	if ((prepTime + cookTime) < 30) {
+		clockStyle = 'green'
+	} else if ((prepTime + cookTime) < 60) {
+		clockStyle = 'yellow'
+	} else {
+		clockStyle = 'red'
+	}
 	return (
 		<div className="post" key={_id}>
-			<div className="post-image">
+			{/* <div className="post-image">
 				<Link to={`/post/${_id}`}>
 					<img src={cover ? `http://localhost:4000/${cover}` : `http://localhost:4000/public/placeholder.png`} alt={title} />
 				</Link>
-			</div>
+			</div> */}
 			<div className="post-content">
 				<Link to={`/post/${_id}`}>
 					<h2 className="text-center">{title}</h2>
@@ -18,8 +26,7 @@ const Post = ({ _id, title, summary, cover, content, author, createdAt, savedBy,
 					<a href="/" className="post-author">{author.username}</a>
 					<time>{format(new Date(createdAt), 'MMM d, yyyy HH:mm')}</time>
 				</p>
-				<p className="post-summary text-center">{summary}
-				</p>
+				<p className="post-time text-center bold"><span className={clockStyle}>{prepTime+cookTime} min</span></p>
 				<SaveButton postId={_id} author={author} savedBy={savedBy} userId={userId} username={userDetails.username} />
 			</div>
 		</div>
