155f8b18e1077678e8dd1420027e6794ebba913d
Lara Alexander <lara.alexander@hotmail.com>
2023-07-17 10:05:43 -0400

    mvc for database

--- server/index.js
+++ server/index.js
@@ -1,37 +1,25 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const mongoose = require('mongoose')
-const User = require('./models/User')
-const Post = require('./models/Post')
 const bcrypt = require('bcryptjs')
-const jwt = require('jsonwebtoken')
 const cookieParser = require('cookie-parser')
-const multer = require('multer')
-const uploadMiddleware = multer({ dest: 'uploads/' })
-const fs = require('fs')
-require('dotenv').config()
-
-const dbConnectionStr = process.env.DB_STRING
+const connectDB = require('./config/database')
+require('dotenv').config({path: './config/.env'})
 
 const PORT = 4000
-
 const salt = bcrypt.genSaltSync(10)
-const secret = 'salkdjfhsk2345rfgd324'
 
 app.use(cors({
     credentials: true,
     origin: 'http://localhost:3000'
 }))
+
+connectDB()
+
 app.use(express.json())
 app.use(cookieParser())
 app.use('/uploads', express.static(__dirname + '/uploads'))
 app.use('/public', express.static(__dirname + '/public'))
-
-const db = (async function () {
-    await mongoose.connect(dbConnectionStr)
-    console.log('Connected to the DB')
-}())
 
 // ***************************** Routers
 
