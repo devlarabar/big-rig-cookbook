5ee7376c5c0cb13105b2c31d1e8ec30aae2dc57a
Lara Alexander <lara.alexander@hotmail.com>
2023-07-18 09:48:01 -0400

    updated user model to include email and admin fields

--- server/models/User.js
+++ server/models/User.js
@@ -2,8 +2,10 @@ const mongoose = require('mongoose')
 const { Schema, model } = mongoose
 
 const UserSchema = new Schema({
-    username: { type: String, required: true, min: 4, unique: true},
-    password: { type: String, required: true,}
+    username: { type: String, required: true, min: 4, unique: true },
+    password: { type: String, required: true },
+    email: { type: String, required: true },
+    admin: { type: Boolean, required: true, default: false }
 })
 
 const UserModel = model('User', UserSchema)
