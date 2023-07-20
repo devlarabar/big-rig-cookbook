a8e6ae7eefc9853d736850f1254950eeb71caa18
Lara Alexander <lara.alexander@hotmail.com>
2023-07-20 08:28:55 -0400

    added the rest of the ingredient svgs

--- client/src/IngredientIcons.js
+++ client/src/IngredientIcons.js
@@ -1,10 +1,16 @@
 // Import SVGs
-import { ReactComponent as Fruit } from './assets/fa-apple-whole-solid.svg'
-import { ReactComponent as Veg } from './assets/fa-seedling-solid.svg'
-import { ReactComponent as Meat } from './assets/fa-drumstick-bite-solid.svg'
-import { ReactComponent as Egg } from './assets/fa-egg-solid.svg'
-import { ReactComponent as Sugar } from './assets/fa-candy-cane-solid.svg'
-import { ReactComponent as Fish } from './assets/fa-fish-fins-solid.svg'
+import { ReactComponent as Fruit } from './assets/ingredients/fa-apple-whole-solid.svg'
+import { ReactComponent as Vegetable } from './assets/ingredients/fa-carrot-solid.svg'
+import { ReactComponent as Meat } from './assets/ingredients/fa-drumstick-bite-solid.svg'
+import { ReactComponent as Egg } from './assets/ingredients/fa-egg-solid.svg'
+import { ReactComponent as Sugar } from './assets/ingredients/fa-candy-cane-solid.svg'
+import { ReactComponent as Fish } from './assets/ingredients/fa-fish-fins-solid.svg'
+import { ReactComponent as Grain } from './assets/ingredients/fa-wheat-awn-solid.svg'
+import { ReactComponent as Bread } from './assets/ingredients/fa-bread-slice-solid.svg'
+import { ReactComponent as Hot } from './assets/ingredients/fa-pepper-hot-solid.svg'
+import { ReactComponent as Cheese } from './assets/ingredients/fa-cheese-solid.svg'
+import { ReactComponent as Spice } from './assets/ingredients/fa-spice-solid.svg'
+import { ReactComponent as Dairy } from './assets/ingredients/fa-cow-solid.svg'
 
 export const IngredientIcons = ({ ingredients }) => {
     const svgClass = 'svg-20'
@@ -25,7 +31,7 @@ export const IngredientIcons = ({ ingredients }) => {
                     case 'fruit':
                         return <Fruit className={svgClass} key={x} />
                     case 'veg':
-                        return <Veg className={svgClass} key={x} />
+                        return <Vegetable className={svgClass} key={x} />
                     case 'meat':
                         return <Meat className={svgClass} key={x} />
                     case 'fish':
@@ -34,6 +40,18 @@ export const IngredientIcons = ({ ingredients }) => {
                         return <Sugar className={svgClass} key={x} />
                     case 'egg':
                         return <Egg className={svgClass} key={x} />
+                    case 'bread':
+                        return <Bread className={svgClass} key={x} />
+                    case 'grain':
+                        return <Grain className={svgClass} key={x} />
+                    case 'hot':
+                        return <Hot className={svgClass} key={x} />
+                    case 'cheese':
+                        return <Cheese className={svgClass} key={x} />
+                    case 'spice':
+                        return <Spice className={svgClass} key={x} />
+                    case 'dairy':
+                        return <Dairy className={svgClass} key={x} />
                     default:
                         return ''
                 }
