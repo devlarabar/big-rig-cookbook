0e14073a1c5431fd31a9a05a02b0ec2c4a44bf11
Lara Alexander <lara.alexander@hotmail.com>
2023-07-24 10:34:42 -0400

    removed unnecessary console.log

--- server/controllers/search.js
+++ server/controllers/search.js
@@ -6,8 +6,6 @@ module.exports = {
         const query = req.params.query
         const ingredient = await Ingredient.find({ name: {$regex: query}})
         const posts = await Post.find({ 'ingredients.ingredient': {$in: ingredient}  }).sort({timestamp: -1})
-        console.log(posts)
-        console.log(ingredient)
         res.json(posts.length > 0 ? posts : null)
     }
 }
