895d40bb1d82d5d0411ce3727170a161b9b74183
Lara Alexander <lara.alexander@hotmail.com>
2023-09-03 11:02:26 -0400

    Vercel troubleshooting

--- server/controllers/home.js
+++ server/controllers/home.js
@@ -1,5 +1,5 @@
 module.exports = {
     test: (req, res) => {
-        res.json(`Server is running on port ${PORT}`)
+        res.json(`Server is running on port ${PORT}`, `Front-end url is ${process.env.FRONTEND_URL}`)
     }
 }
