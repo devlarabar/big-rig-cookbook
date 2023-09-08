5d71957b4467c5fe71c4904dfd485a3b821e2899
Lara Alexander <lara.alexander@hotmail.com>
2023-09-07 20:20:21 -0400

    Changed useProvideAuth request back to fetch

--- client/contexts/AuthContext/useProvideAuth.js
+++ client/contexts/AuthContext/useProvideAuth.js
@@ -7,44 +7,44 @@ const useProvideAuth = () => {
 	const [user, setUser] = useState(null)
 
 	useEffect(() => {
-		// const fetchCurrentUser = async () => {
-			// const response = await fetch(`/auth/getuser`, {
-			// 	method: 'GET',
-			// 	headers: {
-			// 		'Content-Type': 'application/json',
-			// 		'Accept': 'application/json',
-			// 		'Access-Control-Allow-Origin': process.env.NEXT_PUBLIC_FRONTEND_URL
-			// 	},
-			// 	credentials: 'include'
-			// })
-			Axios.defaults.withCredentials = true
-			Axios({
+		const fetchCurrentUser = async () => {
+			const response = await fetch(`/server/auth/getuser`, {
 				method: 'GET',
-				withCredentials: true,
-				url: `/server/auth/getuser`,
-			}).then((res) => {
-				try {
-					if (process.env.NEXT_PUBLIC_ENV === 'local') console.log('Current user:', res.data)
-					console.log('Axios userData:', res.data)
-					setUser(res.data)
-				} catch (err) {
-					// No user
-					setUser(null)
-				}
+				headers: {
+					'Content-Type': 'application/json',
+					'Accept': 'application/json',
+					'Access-Control-Allow-Origin': process.env.NEXT_PUBLIC_FRONTEND_URL
+				},
+				credentials: 'include'
 			})
-			// try {
-			// 	const userData = await response.json()
-			// 	if (process.env.NEXT_PUBLIC_ENV === 'local') console.log('Current user:', userData)
-			// 	setUser(userData)
-			// } catch (err) {
-			// 	// No user
-			// 	setUser(null)
-			// }
-			// if (response instanceof Object) {
-			// 	// To-do: put redirect logic here
-			// }
-		// }
-		// fetchCurrentUser()
+			// Axios.defaults.withCredentials = true
+			// Axios({
+			// 	method: 'GET',
+			// 	withCredentials: true,
+			// 	url: `/server/auth/getuser`,
+			// }).then((res) => {
+			// 	try {
+			// 		if (process.env.NEXT_PUBLIC_ENV === 'local') console.log('Current user:', res.data)
+			// 		console.log('Axios userData:', res.data)
+			// 		setUser(res.data)
+			// 	} catch (err) {
+			// 		// No user
+			// 		setUser(null)
+			// 	}
+			// })
+			try {
+				const userData = await response.json()
+				if (process.env.NEXT_PUBLIC_ENV === 'local') console.log('Current user:', userData)
+				setUser(userData)
+			} catch (err) {
+				// No user
+				setUser(null)
+			}
+			if (response instanceof Object) {
+				// To-do: put redirect logic here
+			}
+		}
+		fetchCurrentUser()
 	}, [])
 
 	const logout = async () => {
